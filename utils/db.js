9fa6ae1a3f1a02f8a2c3cae956bdc4651ab8c200
excellencyjumo <excellencejumo@gmail.com>
2023-10-02 13:22:44 +0100

    ...

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(
   "https://booqwbwszbdfkeqpsbdi.supabase.co",
-  "be680759d2964168f22fbbcecdeb8cd21d76d602"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvb3F3YndzemJkZmtlcXBzYmRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYxOTU5OTQsImV4cCI6MjAxMTc3MTk5NH0.YOypYo4HS57nrdTIkF8itBJ0sp-_3OwR9Laf3v98N2s"
 );
 
 module.exports = supabase;
